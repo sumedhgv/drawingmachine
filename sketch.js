eb3c910458a18a9eaedfbc76c8baa72be77f1464
Sumedh Vedanthi <sumedhgv@gmail.com>
2019-10-08 16:27:01 -0700

    asfsadf

--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
+let noiseOffset = 0.0;
+let strokeWidth = 5;
+
 function setup() {
   createCanvas(600, 600);
   background(50, 120, 250);
+
 }
 
 function draw() {
 
-
+strokeWeight(strokeWidth);
+  noiseOffset += 0.5;
+  strokeWidth = noise(noiseOffset) * 100;
 if (mouseIsPressed) {
   stroke(map(mouseX, 100, 600, 45, 200, true), map(mouseX, 34, 600, 220, 200, true))
   line(mouseX, mouseY, pmouseX, pmouseY);
   variableEllipse(mouseX, mouseY, pmouseX, pmouseY);
 fill( random(255), random(255), random(255), random(255));
+
+
 }
 function variableEllipse(x, y, px, py) {
   let speed = abs(x - px) + abs(y - py);
@@ -18,4 +26,12 @@ function variableEllipse(x, y, px, py) {
   ellipse(x, y, speed, speed);
 }
 
+}
+
+function keyTyped() {
+
+  if (key === 's') {
+    saveCanvas('fileName', 'png')
+  }
+  return false;
 }
