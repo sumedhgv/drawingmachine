b5083a18c4171948f4bfdc3ae6bd1d7212d9a884
Sumedh Vedanthi <sumedhgv@gmail.com>
2019-10-08 16:09:08 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(220, 100, 100);
 }
 
 function draw() {
-  background(220);
+
+
+if (mouseIsPressed) {
+  stroke(map(mouseX, 100, 600, 45, 200, true), map(mouseX, 34, 600, 220, 200, true))
+  line(mouseX, mouseY, pmouseX, pmouseY);
 }
+}
+
+function
