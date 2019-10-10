ef313cef1e41ae701fca567d43ef4a200f45e4b5
Sumedh Vedanthi <sumedhgv@gmail.com>
2019-10-10 15:43:50 -0700

    fl

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-let noiseOffset = 0.0;
-let strokeWidth = 5;
+//let noiseOffset = 0.0;
+//let strokeWidth = 5;
 
 function setup() {
   createCanvas(600, 600);
@@ -9,9 +9,9 @@ function setup() {
 
 function draw() {
 
-strokeWeight(strokeWidth);
-  noiseOffset += 0.5;
-  strokeWidth = noise(noiseOffset) * 100;
+//strokeWeight(strokeWidth);
+  //noiseOffset += 0.5;
+  //strokeWidth = noise(noiseOffset) * 100;
 if (mouseIsPressed) {
   stroke(map(mouseX, 100, 600, 45, 200, true), map(mouseX, 34, 600, 220, 200, true))
   line(mouseX, mouseY, pmouseX, pmouseY);
